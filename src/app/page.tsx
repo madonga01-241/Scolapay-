f1aa9e7216961b0ee2e2e080df79cc74e5a73003
madonga01-241 <ngobokoivad@gmail.com>
2026-08-12 09:29:25 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 import { Role } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
 
